a7e5dcc6f02c964f09a777ea0774c006df11fbb9
atifdelibasic <atif.delibasic@edu.fit.ba>
2020-04-12 01:45:15 +0200

    toDoList

--- ToDoList/todolist.js
+++ ToDoList/todolist.js
@@ -1,27 +1,46 @@
 
-const Item= "Objekat";
+const Item = "Tasks";
 const taskWrap = document.getElementById('new');
 const btnScroll = document.getElementById('btnScroll');
-var LIST = [];
+let LIST = [];
+
+window.addEventListener('load', () => {
+    getLocalStorage(Item);
+})
+
+window.addEventListener('keydown', (e) => {
+    if(e.key == "Enter"){
+        DodajTask();
+    }
+})
+
+window.addEventListener('scroll', () => {
+    ScrollFunction();
+})
 
 taskWrap.addEventListener('click', delOrMark);
-//delete or add line-through(checked)
+// delete or add line-through(checked)
 function delOrMark(e) {
-    let child = e.target.parentElement;
+    let parent = e.target.parentElement;
     if(e.target.className.includes('fa fa-trash')){
-        taskWrap.removeChild(child);
-        LIST.splice(child.id,1);
+        taskWrap.removeChild(parent);
+        LIST.splice(parent.id,1);
         UkloniSve();
         setLocalStorage(Item, LIST);
         getLocalStorage(Item);
     }
     else if(e.target.type == 'checkbox'){
         let checkbox = e.target;
+        let text = checkbox.nextElementSibling;
         if(checkbox.checked){
-            LIST[child.id].zavrseno = true;
+            console.log('cekirano');
+            LIST[parent.id].zavrseno = true;
+            text.style.textDecoration = "line-through";
         }
         else{
-            LIST[child.id].zavrseno = false;
+            console.log('uncekirano');
+            LIST[parent.id].zavrseno = false;
+            text.style.textDecoration = "none";
         }
         setLocalStorage(Item, LIST);
     }
@@ -29,27 +48,27 @@ function delOrMark(e) {
 
 function ClearStorage(){
     localStorage.clear();
-    LIST=[];
+    LIST = [];
     UkloniSve();
 }
 
-function KreirajTask(text, zavrseno=false, id){
-    //create elements
+function KreirajTask(task){
+    // create task
     let tekst = document.createElement('p');
-    tekst.textContent=text;
-    document.getElementById('unos').value='';
+    tekst.textContent = task.tekst;
+    document.getElementById('unos').value = '';
 
     let itemBox = document.createElement('div');
-    itemBox.className ='task';
-    itemBox.id = id;
+    itemBox.className = 'task';
+    itemBox.id = task.id;
 
     let inputCheck = document.createElement('input');
     inputCheck.type ='checkbox';
 
     let trash = document.createElement('i');
-    trash.className ='fa fa-trash';
+    trash.className = 'fa fa-trash';
    
-    if(zavrseno){
+    if(task.zavrseno){
        SetLineThrough(inputCheck, tekst);
     }
 
@@ -57,25 +76,29 @@ function KreirajTask(text, zavrseno=false, id){
     itemBox.appendChild(tekst);
     itemBox.appendChild(trash);
     taskWrap.appendChild(itemBox);
+}
 
-}
 function SetLineThrough(inputCheck, text){
-    text.style.textDecoration="line-through";
-    inputCheck.checked=true;
+    text.style.textDecoration = "line-through";
+    inputCheck.checked = true;
 }
+
 function UkloniSve(){
     while(taskWrap.hasChildNodes()){
         taskWrap.removeChild(taskWrap.childNodes[0]);
     }
 }
+
 function DodajTask(){
-    var inputValue=document.getElementById('unos').value.trim();
-    if(inputValue!=""){
-        KreirajTask(inputValue, false, LIST.length);
+    let inputValue = document.getElementById('unos').value.trim();
+    if(inputValue!=''){
+        // KreirajTask(inputValue, false, LIST.length);
             LIST.push({
                 tekst:inputValue,
-                zavrseno:false
+                zavrseno:false,
+                id:LIST.length
             });
+            KreirajTask(LIST[LIST.length-1]);
         setLocalStorage(Item, LIST);
     }
 } 
@@ -85,38 +108,25 @@ function setLocalStorage(key,value){
 }
 
 function getLocalStorage(item){
-    var data= localStorage.getItem(item);
+    var data = localStorage.getItem(item);
     if(data){
-        LIST=JSON.parse(data);
+        LIST = JSON.parse(data);
         UcitajTaskove();
     }
 }
 
 function UcitajTaskove(){
-    for(var i=0; i<LIST.length; i++){
-        KreirajTask(LIST[i].tekst, LIST[i].zavrseno,i);
-    }
-}
-
-window.onload = () =>{
-    getLocalStorage(Item);
-}
-
-window.onkeydown = e => {
-    if(e.key=="Enter"){
-        DodajTask();
-    }
-}
-
-window.onscroll= () =>{
-    ScrollFunction();
+    LIST.forEach((task, index) => {
+        task.id = index;
+        KreirajTask(task);
+    });
 }
 
 function ScrollFunction(){
     if(document.documentElement.scrollTop > 20){
-        btnScroll.style.display="block";
+        btnScroll.style.display = "block";
     }
     else{
-        btnScroll.style.display="none";
+        btnScroll.style.display = "none";
     }
 }
